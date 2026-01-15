1898e3935dbbc97deea6e890a38309b1bbb23c24
Sirtec <sirtec@sirtecroute.com>
2026-01-15 12:50:48 -0300

    Backup agendado - 2026-01-15 12:50

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-800fb68e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.erjq9rbra9o"
+    "revision": "0.p777bangcg4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
